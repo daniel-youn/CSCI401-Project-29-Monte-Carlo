6162769fb4886c26efce6e3e6f98cf8c7df278da
Roy Choi <rschoi@usc.edu>
2024-10-07 14:50:13 -0700

    Added page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import MyProjectsPage from './pages/MyProjectsPage/MyProjectsPage';
+
 function App() {
   return (
-    <MyProjectsPage></MyProjectsPage>
+    <Router>
+      <Routes>
+        {/* Route for Login Page */}
+        <Route path="/" element={<LoginPage />} />
+
+        {/* Route for Registration Page */}
+        <Route path="/register" element={<RegistrationPage />} />
+
+        {/* Route for My Projects Page */}
+        <Route path="/projects" element={<MyProjectsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
