6a2d77d090c8d12785531e82a4ad01ce272d4e9e
Roy Choi <rschoi@usc.edu>
2024-10-20 17:33:03 -0700

    Editted graph form and added myprojects page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,18 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-import MyProjectsPage from './pages/MyProjectsPage/MyProjectsPage';
 import FormPage from './pages/FormPage/FormPage';
 import ProjectViewPage from './pages/ProjectViewPage/ProjectViewPage'
-
+import MyProjectsPage from './pages/MyProjectsPage/MyProjectsPage';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/projects" element={<MyProjectsPage />} />
         <Route path="/form" element={<FormPage />} />
         <Route path="/project-page" element={<ProjectViewPage />} />
-
+        <Route path="/my-projects-page" element={<MyProjectsPage />} />
       </Routes>
     </Router>
   );
