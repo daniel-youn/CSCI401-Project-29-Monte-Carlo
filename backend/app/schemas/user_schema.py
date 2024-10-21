3c5dd4ccb94d50e7a680fcfa5e667feb3f78d53b
Seeon Chang <zionchan@usc.edu>
2024-10-20 17:06:36 -0700

    dog

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -2,23 +2,13 @@ from marshmallow import Schema, fields, validate
 
 class UserSchema(Schema):
     user_id = fields.Str(validate=validate.Regexp(r'^[a-zA-Z0-9._@]+$', error="user_id must be a valid email format or alphanumeric with underscores, dots, and @ allowed"))
-    first_name = fields.Str(validate=[
-        validate.Length(min=2, max=50),
-        validate.Regexp(r'^[a-zA-Z]+$', error="First name must only contain letters")
-    ], required=False)  # Optional
-    last_name = fields.Str(validate=[
-        validate.Length(min=2, max=50),
-        validate.Regexp(r'^[a-zA-Z]+$', error="Last name must only contain letters")
-    ], required=False)  # Optional
-    # password = fields.Str(required=True, validate=[
-    #     validate.Length(min=8, error="Password must be at least 8 characters long"),
-    #     validate.Regexp(r'(?=.*[0-9])(?=.*[!@#$%^&*])', 
-    #                     error="Password must include at least one number and one special character")
-    # ])
+    first_name = fields.Str(required=True) 
+    last_name = fields.Str(required=True) 
     password = fields.Str(required=True)
     simulations = fields.List(fields.Str(validate=validate.Length(equal=24)), required=False)  # Optional
     email = fields.Email(required=True)
     settings = fields.Dict(required=False)  # Optional
+    isAdmin = fields.Bool(required=False) # default false
 
 # Create an instance of the UserSchema
 user_schema = UserSchema()
